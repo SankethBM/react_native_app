621b7fd875c685c4a4b0f00d85194620c8550ceb
sanketh <sankethbm333@gmail.com>
2024-08-18 19:57:34 +0530

    profile section done

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import UserIntro from "../../components/Profile/UserIntro";
+import MenuList from "../../components/Profile/MenuList";
 
 export default function profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View
+      style={{
+        paddingTop: 50,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 30,
+          textAlign: "center",
+        }}
+      >
+        Profile
+      </Text>
+
+      {/* User Info */}
+      <UserIntro />
+
+      {/* Menu List */}
+      <MenuList />
     </View>
-  )
+  );
 }
---
+++ components/Profile/UserIntro.jsx
@@ -0,0 +1,44 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+
+export default function UserIntro() {
+  const { user } = useUser();
+  return (
+    <View
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 30,
+      }}
+    >
+      <Image
+        source={{ uri: user?.imageUrl }}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 99,
+        }}
+      />
+
+      <Text
+        style={{
+          fontSize: 20,
+          fontFamily: "outfit-bold",
+        }}
+      >
+        {user?.fullName}
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: "outfit",
+          fontSize: 16,
+        }}
+      >
+        {user?.primaryEmailAddress?.emailAddress}
+      </Text>
+    </View>
+  );
+}
